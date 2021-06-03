b64e6265536756e4633397c2055ba01c1634b52a
thanhdat10 <85265611+thanhdat10@users.noreply.github.com>
2021-06-03 09:14:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Okiii anh yêu❤',
-    title: 'Phải chăng em đã yêu anh từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng em đã yêu anh từ cái nhìn đầu tiên 😚',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là yêu anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3?',
-    btnReply: 'Gửi cho anh <3',
-    reply: '                             ',
-    mess: 'Anh biết mà 🥰. Yêu em 😘😘',
-    messDesc: 'Anh yêu em nhiều nhiều❤❤ .',
-    btnAccept: 'Okiiiii iuuu <3',
+    btnReply: 'Đơn giản là yêu anh thôi😚',
+    mess: 'Ỏ iuuuu emmm😘🥰',
+    messDesc: 'I love you,bae❤❤ .',
+    btnAccept: 'Ỏ love you too🥺 <3',
     messLink: 'https://www.facebook.com/dat.thanh.nguyen.4/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
